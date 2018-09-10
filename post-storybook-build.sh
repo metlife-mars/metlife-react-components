46081feb86cc299d9cd6be512a7a2d45d2e16851
Marcellin Nshimiyimana <mars.nshimiyimana@metlife.com>
2018-09-10 19:42:41 -0400

    try append base to the built html

--- post-storybook-build.sh
+++ post-storybook-build.sh
@@ -1,1 +1,1 @@
-npx replace-in-file '<title>Storybook</title>' '<title>MetLife - React Components</title><base href = "https://metlife-mars.github.io/metlife-react-components/" />'  storybook-static/*.html
+npx replace-in-file '<title>Storybook</title>' '<title>MetLife - React Components</title><base href="https://metlife-mars.github.io/metlife-react-components/" />'  storybook-static/*.html
